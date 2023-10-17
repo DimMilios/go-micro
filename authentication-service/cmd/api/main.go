916e5d1099495c84488ac6ee5052c5aae0d15e17
DimMilios <dimitris.milios96@gmail.com>
2023-10-17 10:46:49 +0300

    setup authentication service

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
